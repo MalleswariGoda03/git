b7179622abe09ae488e5933744b8ba7005fe1702
MalleswariGoda03 <malleswarigoda004@gmail.com>
2025-06-30 12:38:37 +0530

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,18 @@
+class Bank:
+    def _init_(self):
+        self.__balance=0
+    def deposit(self,amount):
+        self.__balance+=amount
+    def withdraw(self,amount):
+        if  amount >self.__balance or amount<0:
+            print("Insufficient")
+        else:
+            self.__balance-=amount
+    def get_balance(self):
+        return self.__balance
+b=Bank()
+b.deposit(1000)
+b.deposit(1200)
+b.withdraw(-100)
+b.withdraw(2300)
+print(b.get_balance())
